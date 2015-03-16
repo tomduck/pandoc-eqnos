0bb844034a3c8970c3661148099f8ea4e2c4a280
Thomas Duck <tomduck@tomduck.ca>
2015-03-15 22:06:29 -0300

    Linting.

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
+"""setup.py - install script for pandoc-eqnos."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-long_description = """\
+LONG_DESCRIPTION = """\
 pandoc-eqnos is a pandoc filter for numbering equations and equation references.
 """
 
 setup(
-    name = 'pandoc-eqnos',
-    version = '0.1',
+    name='pandoc-eqnos',
+    version='0.1',
 
-    author = 'Thomas J. Duck',
-    author_email = 'tomduck@tomduck.ca',
-    description = 'Equation number filter for pandoc',
-    long_description=long_description,
-    license = 'GPL',
-    keywords = 'pandoc equation numbers filter',
+    author='Thomas J. Duck',
+    author_email='tomduck@tomduck.ca',
+    description='Equation number filter for pandoc',
+    long_description=LONG_DESCRIPTION,
+    license='GPL',
+    keywords='pandoc equation numbers filter',
     url='https://github.com/tomduck/pandoc-eqnos',
 
     install_requires=['pandocfilters', 'pandoc-attributes'],
 
-    py_modules = ['pandoc_eqnos'],
-    entry_points = { 'console_scripts':
-                     ['pandoc-eqnos = pandoc_eqnos:main'] },
+    py_modules=['pandoc_eqnos'],
+    entry_points={'console_scripts':['pandoc-eqnos = pandoc_eqnos:main']},
 
-    classifiers = [
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: End Users/Desktop',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python' ]
+        'Programming Language :: Python'
+        ]
 )
