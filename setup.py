511d3226aa2e73e5f0e8a8fb3b35b7e03396b494
Thomas Duck <tomduck@tomduck.ca>
2015-07-17 12:03:49 -0300

    Bumped version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ pandoc-eqnos is a pandoc filter for numbering equations and equation references.
 
 setup(
     name='pandoc-eqnos',
-    version='0.3',
+    version='0.4',
 
     author='Thomas J. Duck',
     author_email='tomduck@tomduck.ca',
@@ -20,7 +20,7 @@ setup(
     license='GPL',
     keywords='pandoc equation numbers filter',
     url='https://github.com/tomduck/pandoc-eqnos',
-    download_url = 'https://github.com/tomduck/pandoc-eqnos/tarball/0.3',
+    download_url = 'https://github.com/tomduck/pandoc-eqnos/tarball/0.4',
 
     install_requires=['pandocfilters', 'pandoc-attributes'],
 
