57e4d7cc1fb46f67af8391dfe2226b8bd1c5b4ca
Thomas Duck <tomduck@tomduck.ca>
2016-05-18 10:10:19 -0300

    Bumped xnos version required.

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     url='https://github.com/tomduck/pandoc-eqnos',
     download_url='https://github.com/tomduck/pandoc-eqnos/tarball/' + VERSION,
 
-    install_requires=['pandoc-xnos>=0.2',
+    install_requires=['pandoc-xnos>=0.3',
                       'pandocfilters',
                       'pandoc-attributes'],
 
